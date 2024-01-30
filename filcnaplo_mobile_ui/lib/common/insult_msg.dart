f5e23f596a63f0a937b41d6fba5ed17df91d8b4b
Balázs(Manus) <hegebalazs.7919@gmail.com>
2024-01-30 16:36:35 +0100

    idk if this works

--- filcnaplo_mobile_ui/lib/common/insult_msg.dart
+++ filcnaplo_mobile_ui/lib/common/insult_msg.dart
@@ -1,1 +1,43 @@
 // Armand ide írjad a kódot, majd valahogy belinkelem a kódba xddd
+import 'dart:math';
+import 'dart:io';
+
+List<String> randomInsults = [];
+
+Random random = Random();
+
+// Read dirtywords
+File file = File('dirtywords.xml');
+List<String> dirtyWords = file.readAsLinesSync();
+
+List<String> mWords = [];
+List<String> fWords = [];
+
+// Put m and f type strings into list
+for (String word in dirtyWords) {
+    if (word.contains('type="m"')) {
+        mWords.add(word);
+    } else if (word.contains('type="f"')) {
+        fWords.add(word);
+    }
+}
+
+while (randomInsults.length < 3) {
+    if (randomInsults.length < 2) {
+        int index = random.nextInt(mWords.length);
+        String insult = mWords[index];
+        if (!randomInsults.contains(insult)) {
+            randomInsults.add(insult);
+        }
+    } else {
+        int index = random.nextInt(fWords.length);
+        String insult = fWords[index];
+        if (!randomInsults.contains(insult)) {
+            randomInsults.add(insult);
+        }
+    }
+}
+
+String generatedString = randomInsults.join(', ');
+
+print(generatedString); // output for testing purposes
